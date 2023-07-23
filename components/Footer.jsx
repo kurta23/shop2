29319d7caf10816ab51e007a54441a637a3a494b
kurta23 <138971206+kurta23@users.noreply.github.com>
2023-07-23 17:09:58 +0200

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,9 +4,9 @@ import {AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai'
 const Footer = () => {
   return (
     <div className='footer-container'>
-      <p>2023 Kurtovic|Art All rights reserved</p>
+      <p>2023 AnakuArt All rights reserved</p>
       <p className='icons'>
-        <a href="https://instagram.com/kurtovicanamaria?igshid=YmMyMTA2M2Y=">
+        <a href="https://www.instagram.com/mirno.idem.krivim.putem/">
           <AiFillInstagram />   
         </a> 
       </p>
