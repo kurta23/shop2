d9e48bd4ce45cb2959c7e41e3623ed4eebad9c72
kurta23 <138971206+kurta23@users.noreply.github.com>
2023-07-25 11:33:18 +0200

    Update stripe.js

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
         shipping_address_collection: {
-          allowed_countries: ['HR'],['DE'],
+          allowed_countries: ['HR', 'DE'],
         },
         shipping_options: [
           {shipping_rate: 'shr_1NXhCKBKUFdv47TESPeFZKdH'},
